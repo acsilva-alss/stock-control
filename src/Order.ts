5087fd11b8b1887181ee0459ccdb30d077b8840e
Alisson Chagas Silva <alisson.cs@hotmail.com>
2023-08-23 09:48:14 -0300

    Add order code logic

--- src/Order.ts
+++ src/Order.ts
@@ -1,9 +1,16 @@
 import Cpf from './Cpf'
 
 export default class Order{
+  readonly orderCode: string
   cpf: Cpf
-
-  constructor(userCPF: string){
+  constructor(userCPF: string, readonly orderDate: Date, sequency = 1){
     this.cpf = new Cpf(userCPF)
+    this.orderCode = this.generateOrderCode(orderDate, sequency)
+  }
+
+  private generateOrderCode(date: Date,sequency: number): string {
+    const orderDateYear = date.getFullYear()
+    const orderSequency = new String(sequency).padStart(8, '0')
+    return  `${orderDateYear}${orderSequency}`
   }
 }
--- test/Order.test.ts
+++ test/Order.test.ts
@@ -2,6 +2,11 @@ import Order from "../src/Order"
 
 describe('Tests on order entity', () => {
   test('Should not create a order with invalid CPF', () => {
-    expect(() => new Order('892534')).toThrow(new Error('Invalid CPF'))
+    expect(() => new Order('892534', new Date())).toThrow(new Error('Invalid CPF'))
+  })
+
+  test('Should create a order with code equal 202300000001', () => {
+    const order = new Order('056.196.430-00', new Date('2023-03-10T10:00:00'))
+    expect(order.orderCode).toBe('202300000001')
   })
 })
